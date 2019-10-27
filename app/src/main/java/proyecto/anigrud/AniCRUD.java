7d5d27d75921bd61ba019f9f4889f68d950ad787
Antonio .M.D <teclado1333@gmail.com>
2019-10-27 20:07:08 +0100

    agregando scroll para el formulario

--- app/src/main/java/proyecto/anigrud/AniCRUD.java
+++ app/src/main/java/proyecto/anigrud/AniCRUD.java
@@ -6,14 +6,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
-import proyecto.anigrud.views.ListadoActivity;
+import proyecto.anigrud.views.*;
+
 
 public class AniCRUD extends Activity {
+    String TAG = "aniGRUD/MainActivitiy";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        Log.i(TAG,"entrado en el oncreate");
         setContentView(R.layout.activity_ani_crud);
 
         new Handler().postDelayed(
@@ -26,9 +32,33 @@ public class AniCRUD extends Activity {
                     }
                 },5000
 
-
         );
 
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"entrado en el onStart");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"entrado en el onresume");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"entrado en el stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"entrado en el stop");
     }
 }
