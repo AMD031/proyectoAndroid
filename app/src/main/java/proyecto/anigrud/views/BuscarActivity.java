3cc1497b6ce4c9c975051e52d11931d5564bf929
Antonio Martinez Diaz <teclado1333@gmail.com>
2019-11-18 14:48:38 +0100

    agregando datepicker

--- app/src/main/java/proyecto/anigrud/views/BuscarActivity.java
+++ app/src/main/java/proyecto/anigrud/views/BuscarActivity.java
@@ -1,5 +1,6 @@
 package proyecto.anigrud.views;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,10 +10,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+
+import java.util.Calendar;
 
 import proyecto.anigrud.R;
 
-public class BuscarActivity extends AppCompatActivity {
+public class BuscarActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageButton btnFecha;
+    private EditText etFecha;
+    private int dia, mes, agno, hora, minutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +31,29 @@ public class BuscarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_buscar);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+       btnFecha = (ImageButton) findViewById(R.id.btnFecha);
+       etFecha = (EditText) findViewById(R.id.etFecha);
+       etFecha.setOnClickListener(this);
+
     }
 
 
+    @Override
+    public void onClick(View v) {
+        if(v==btnFecha){
+            final Calendar c= Calendar.getInstance();
+            this.dia = c.get(Calendar.DAY_OF_MONTH);
+            this.mes = c.get(Calendar.MONTH);
+            this.agno = c.get(Calendar.YEAR);
 
-
-
-
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        etFecha.setText(dayOfMonth+""+month+""+year);
+                }
+            },dia,mes,agno);
+            datePickerDialog.show();
+        }
+    }
 }
