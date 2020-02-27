f1e57667e2191d01c3f13ebf76a17c1161d741b9
unknown <teclado1333@gmail.com>
2020-02-27 21:42:16 +0100

    agregando webView

--- app/src/main/java/proyecto/anigrud/views/AyudaActivity.java
+++ app/src/main/java/proyecto/anigrud/views/AyudaActivity.java
@@ -1,7 +1,14 @@
 package proyecto.anigrud.views;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,7 +17,7 @@ import androidx.appcompat.widget.Toolbar;
 import proyecto.anigrud.R;
 
 public class AyudaActivity extends AppCompatActivity {
-
+    private WebView mWebview ;
     private String ayuda;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,28 @@ public class AyudaActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         this.ayuda = getIntent().getStringExtra("ayuda");
         Log.i("buscar", ayuda);
+        mWebview  = new WebView(this);
+
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+
+        final Activity activity = this;
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                // Redirect to deprecated method, so you can use it in all SDK versions
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+        });
+
+        mWebview .loadUrl("http://www.google.com");
+        setContentView(mWebview );
 
 
 
